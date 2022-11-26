de1a8606ce7e08c67d0349dc1c57c900b0dc419a
transwold6969 <119092014+transwold6969@users.noreply.github.com>
2022-11-26 10:45:39 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,68 +1,44 @@
 let pp = 2;
 let i = 1;
-function createVideoPage(vid_id){   console.log(vid_id);            }
-function createHome(pg) {
-    fetch("https://www.eporner.com/api/v2/video/search/?per_page=50&order=latest&page=" + pg)
-        .then((resp) => resp.json())
-        .then((data) => {
-            $(".preloader").remove();
-            const videos = data.videos;
-            videos.map(function(video) { $("#v").append(`
-   
-      <div id="${video.id}" onclick="createVideoPage(${video.id}" class="col-sm-3 col-md-3 col-lg-3">
-      <img class="img-fluid" src="${video.default_thumb.src}" alt="${video.title} thumbnail">
-        <h5 class="card-title">  
-         <a href="${video.url}" target="__blank" class="text-dark text-break" style="text-decoration:none;" /> 
-           ${video.title}
-        </a>
-        </h5>
-                    
+
+function ie(url,title) { 
+ $("#modal_div").html(`
+<div id="Modal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+<!-- Modal content-->
+<div class="modal-content">
+<div class="modal-header text-center">
+ <p>${title}</p>
+</div>
+  <div class="modal-body">
+    <img src="${url}" class="img-thumbnail" alt="${title}">
+  </div>  
+<div class="modal-footer">
+ <a href="${url}" class="btn btn-danger" download target="__blank">Download</a>
+</div>
+</div>
   </div>
-   `)
-            });
-        });
+</div>
+`);
+ $("#Modal").modal('show');
 };
 
-
-function filterVideo(pg) {
-  var filt = $( "#filter" ).val();
-  
-    fetch("https://www.eporner.com/api/v2/video/search/?per_page=20&order=" + filt +"&page=" + pg)
-        .then((resp) => resp.json())
-        .then((data) => {
-            $(".preloader").remove();
-            const videos = data.videos;
-            videos.map(function(video) {
-              
-                $(".longest").append(`
-   
-      <div class="col-sm-3 col-md-3 col-lg-3">
-       <img class="img-fluid" src="${video.default_thumb.src}" alt="${video.title} thumbnail">
-        <h5 class="card-title">  
-         <a href="${video.url}" target="__blank" class="text-dark text-break" style="text-decoration:none;" /> 
-           ${video.title}
-        </a>
-        </h5>
+function createHome(pg) { 
+fetch("https://www.eporner.com/api/v2/video/search/per_page=1000&lq=0    &order=latest&gay=0&page="+pg )    .then(resp=> resp.json())       .then((data) => {            $(".preloader").remove();            const videos = data.videos;
+ videos.map(function(video) {
+  $("#v").append(`
+      <div class="col-sm-3 py-3 col-md-3 col-lg-3 image-div idv">
+       <img class="img-fluid" src="${video.default_thumb.src}" alt="${video.title}" onClick="ie('${video.default_thumb.src}','${video.title}');">
+       <h2>${video.title}</h2>
   </div>
-   `)
-            });
-        });
+   `);
+        
+ });   
+ $(".idv").removeClass("image-div" );                                                                                              
+});
+      
 };
-$('#filter').on('change', function() {
-  $(".longest").empty();        filterVideo(pp);
-    ++pp;
-  
-});
-
-filterVideo(1)
-++pp;
-
-
-$("#long-show").click(function() {
-  filterVideo(pp);
-  ++pp;
-});
-
 
 createHome(i);
 ++i;
@@ -77,5 +53,12 @@ $(window).scroll(function() {
      createHome(i);
     ++i;
    }
-});                                     $(window).on('load', function () {
-     $("#cover").fadeOut(1500);           })
+});
+setInterval(function () {
+  createHome(i);
+   ++i
+   
+}, 5000);
+
+
+
